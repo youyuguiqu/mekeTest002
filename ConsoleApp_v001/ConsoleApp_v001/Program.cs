be3990f5981160111bfefb6c6def1b4fb2502bb4
lijg <1508463510@qq.com>
2018-12-13 12:03:25 +0800

    hello world

---
+++ ConsoleApp_v001/ConsoleApp_v001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_v001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.ReadLine();
+        }
+    }
+}
